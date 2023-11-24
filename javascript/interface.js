9a4fe76e8aca3607323cb7de892d3f4efda679c7
Antonio Bertuccio <anto.bertu@gmail.com>
2023-11-24 11:46:33 +0100

    first commit

---
+++ javascript/interface.js
@@ -0,0 +1,44 @@
+var User = /** @class */ (function () {
+    function User(nome, cognome, numeroTelefono, carica, numeroChiamate) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numeroTelefono = numeroTelefono;
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+    }
+    User.prototype.ricarica = function (unaRicarica) {
+        this.carica += unaRicarica;
+        console.log("Ricarica effettuata. Nuovo saldo carica: \u20AC".concat(this.carica.toFixed(2)));
+    };
+    User.prototype.chiamata = function (minutiDurata) {
+        if (this.carica >= minutiDurata) {
+            var costoChiamata = minutiDurata * 0.2;
+            this.carica -= costoChiamata;
+            this.numeroChiamate++;
+            console.log("Chiamata effettuata per ".concat(minutiDurata, " minuti."));
+            console.log("Costo della chiamata: \u20AC".concat(costoChiamata.toFixed(2)));
+            console.log("Saldo carica rimanente: \u20AC".concat(this.carica.toFixed(2)));
+        }
+        else {
+            console.log("Saldo carica insufficiente per effettuare la chiamata.");
+        }
+    };
+    User.prototype.numero404 = function () {
+        console.log("Credito residuo: ".concat(this.carica, "\u20AC"));
+    };
+    User.prototype.getNumeroChiamate = function () {
+        console.log("Cronologia chiamate: ".concat(this.numeroChiamate));
+    };
+    User.prototype.azzeraChiamate = function () {
+        this.numeroChiamate = 0;
+        console.log("Cronologia chiamate cancellata.");
+    };
+    return User;
+}());
+var FirstUser = new User("John", "Doe", "123-456-7890", 16.0, 12);
+console.log("User: ".concat(FirstUser.nome, " ").concat(FirstUser.cognome));
+FirstUser.numero404();
+FirstUser.chiamata(5);
+FirstUser.numero404();
+FirstUser.getNumeroChiamate();
+FirstUser.azzeraChiamate();
---
+++ javascript/interface.ts
@@ -0,0 +1,71 @@
+interface Smartphone {
+  numeroTelefono: string;
+  carica: number;
+  numeroChiamate: number;
+
+  ricarica(unaRicarica: number): void;
+  chiamata(minutiDurata: number): void;
+}
+
+class User implements Smartphone {
+  nome: string;
+  cognome: string;
+  numeroTelefono: string;
+  carica: number;
+  numeroChiamate: number;
+
+  constructor(
+    nome: string,
+    cognome: string,
+    numeroTelefono: string,
+    carica: number,
+    numeroChiamate: number
+  ) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.numeroTelefono = numeroTelefono;
+    this.carica = carica;
+    this.numeroChiamate = numeroChiamate;
+  }
+
+  ricarica(unaRicarica: number): void {
+    this.carica += unaRicarica;
+    console.log(
+      `Ricarica effettuata. Nuovo saldo carica: €${this.carica.toFixed(2)}`
+    );
+  }
+
+  chiamata(minutiDurata: number): void {
+    if (this.carica >= minutiDurata) {
+      const costoChiamata = minutiDurata * 0.2;
+      this.carica -= costoChiamata;
+      this.numeroChiamate++;
+      console.log(`Chiamata effettuata per ${minutiDurata} minuti.`);
+      console.log(`Costo della chiamata: €${costoChiamata.toFixed(2)}`);
+      console.log(`Saldo carica rimanente: €${this.carica.toFixed(2)}`);
+    } else {
+      console.log(`Saldo carica insufficiente per effettuare la chiamata.`);
+    }
+  }
+
+  numero404(): void {
+    console.log(`Credito residuo: ${this.carica}€`);
+  }
+
+  getNumeroChiamate(): void {
+    console.log(`Cronologia chiamate: ${this.numeroChiamate}`);
+  }
+
+  azzeraChiamate(): void {
+    this.numeroChiamate = 0;
+    console.log(`Cronologia chiamate cancellata.`);
+  }
+}
+
+const FirstUser = new User("John", "Doe", "123-456-7890", 16.0, 12);
+console.log(`User: ${FirstUser.nome} ${FirstUser.cognome}`);
+FirstUser.numero404();
+FirstUser.chiamata(5);
+FirstUser.numero404();
+FirstUser.getNumeroChiamate();
+FirstUser.azzeraChiamate();
